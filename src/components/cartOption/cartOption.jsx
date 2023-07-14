13a555ae898b47153820b77fe8e83be0b1d6bbe9
Gustavo <gustavoob06@gmail.com>
2023-07-14 15:45:20 -0300

    pequeno ajuste

--- src/components/cartOption/cartOption.jsx
+++ src/components/cartOption/cartOption.jsx
@@ -5,7 +5,7 @@ export default function CartOption({image,title,price,onRemove }){
         <div className={styles.option}>
             <Image className={styles.image} src={`/products/${image}`} alt={`Produto ${image}`} width={62} height={74}/>
             <div className={styles.info}>
-                <h3 className={styles.title}>{name}</h3>
+                <h3 className={styles.title}>{title}</h3>
                 <h3 className={styles.price}>R${price.toFixed(2)}</h3>
                 <p className={styles.remove} onClick={onRemove}>remover</p>
             </div>
